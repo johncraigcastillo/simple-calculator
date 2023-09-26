7f9c9e555a16d67a91a0ff35e94963e7863516ce
johncastillo <johncraigcastillo@gmail.com>
2023-09-25 22:33:49 -0500

    fully functional, separated into two projects

---
+++ CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,80 @@
+﻿using Spectre.Console;
+
+namespace CalculatorLibrary;
+
+public class Calculator
+{
+    private int _num1;
+    private int _num2;
+    private string? _operation;
+    private double _result;
+    private string[]? _operationList;
+
+    private static void Quit()
+    {
+        AnsiConsole.MarkupLine("[bold blue]Goodbye![/]");
+        Environment.Exit(0);
+    }
+
+    public void Run()
+    {
+        AnsiConsole.MarkupLine("[bold blue]============================[/]");
+        AnsiConsole.MarkupLine("[bold blue]C# Command Line Calculator[/]");
+        AnsiConsole.MarkupLine("[bold blue]============================[/]");
+        SetNumbers();
+    }
+
+    private void SetNumbers()
+    {
+        _num1 = AnsiConsole.Ask<int>("Enter the [bold blue]first number[/]: ");
+        _num2 = AnsiConsole.Ask<int>("Enter the [bold blue]second number[/]: ");
+        SelectOperation();
+    }
+
+    private void SelectOperation()
+    {
+        ZeroDivisionProtection();
+
+        var prompt = new SelectionPrompt<string>()
+            .Title("[bold blue]Select an operation[/]");
+        foreach (var op in _operationList)
+        {
+            prompt.AddChoices(op);
+        }
+
+        _operation = AnsiConsole.Prompt(prompt);
+
+        Calculate();
+    }
+
+    private void ZeroDivisionProtection()
+    {
+        _operationList = _num2 == 0
+            ? new[] { "Add", "Subtract", "Multiply", "Quit" }
+            : new[] { "Add", "Subtract", "Multiply", "Divide", "Quit" };
+    }
+
+    private void Calculate()
+    {
+        if (_operation == "Quit")
+        {
+            Quit();
+        }
+
+        _result = _operation switch
+        {
+            "Add" => _num1 + _num2,
+            "Subtract" => _num1 - _num2,
+            "Multiply" => _num1 * _num2,
+            "Divide" => _num2 > 0 ? Convert.ToDouble(_num1) / _num2 : 0,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+        DisplayResult();
+    }
+
+    private void DisplayResult()
+    {
+        AnsiConsole.MarkupLine($"[bold blue]Result:[/] {_result}");
+        Run();
+    }
+}
--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -1,4 +1,4 @@
-﻿using Spectre.Console;
+﻿using CalculatorLibrary;
 
 namespace SimpleCalculator;
 
@@ -7,58 +7,7 @@ public static class Program
     private static void Main(string[] args)
     {
         var calculator = new Calculator();
-        calculator.Start();
+        calculator.Run();
     }
 }
 
-internal class Calculator
-{
-    private int _num1;
-    private int _num2;
-    private string? _operation;
-    private int _result;
-
-    public void Start()
-    {
-        AnsiConsole.MarkupLine("[bold blue]C# Command Line Calculator[/]");
-        AnsiConsole.MarkupLine("[bold blue]============================[/]");
-        SetNumbers();
-    }
-
-    private void SetNumbers()
-    {
-        _num1 = AnsiConsole.Ask<int>("Enter the [bold blue]first number[/]: ");
-        _num2 = AnsiConsole.Ask<int>("Enter the [bold blue]second number[/]: ");
-        SelectOperation();
-    }
-
-    private void SelectOperation()
-    {
-        _operation = AnsiConsole.Prompt(
-            new SelectionPrompt<string>()
-                .Title("[bold blue]Select an operation[/]")
-                .AddChoices(new[]
-                {
-                    "Add", "Subtract", "Multiply", "Divide"
-                }));
-        Calculate();
-    }
-
-    private void Calculate()
-    {
-        _result = _operation switch
-        {
-            "Add" => _num1 + _num2,
-            "Subtract" => _num1 - _num2,
-            "Multiply" => _num1 * _num2,
-            "Divide" => _num1 / _num2,
-            _ => throw new ArgumentOutOfRangeException()
-        };
-        DisplayResult();
-    }
-
-    private void DisplayResult()
-    {
-        AnsiConsole.MarkupLine($"[bold blue]Result:[/] {_result}");
-    }
-}
